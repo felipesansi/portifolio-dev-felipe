35f90cc06409536255c5b80b69e6eb9bc01a8c68
Felipe Freitas dos Santos Silva <82713076+felipesansi@users.noreply.github.com>
2024-07-16 15:05:07 -0300

    repositório criado

---
+++ Js/main.js
@@ -0,0 +1,18 @@
+function AbrirMenu() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('abrir')) {
+      menuMobile.classList.remove('abrir');
+  } else {
+      menuMobile.classList.add('abrir');
+  }
+}
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('abrir')) {
+      menuMobile.classList.remove('abrir');
+      document.querySelector('.icone').src = "/Imagens/Icone-menu.svg";
+  } else {
+      menuMobile.classList.add('abrir');
+      document.querySelector('.icone').src = "/Imagens/Icone-menu-fechar.svg";
+  }
+}
