0ceb232f7bc07f7ca01cdeecef02c4d179237f88
Felipe Freitas dos Santos Silva <82713076+felipesansi@users.noreply.github.com>
2024-07-18 15:20:35 -0300

    Habilidades

--- Js/main.js
+++ Js/main.js
@@ -10,9 +10,9 @@ function menuShow() {
   let menuMobile = document.querySelector('.mobile-menu');
   if (menuMobile.classList.contains('abrir')) {
       menuMobile.classList.remove('abrir');
-      document.querySelector('.icone').src = "/Imagens/Icone-menu.svg";
+      document.querySelector('.icone').src = "./Imagens/Icone-menu.svg";
   } else {
       menuMobile.classList.add('abrir');
-      document.querySelector('.icone').src = "/Imagens/Icone-menu-fechar.svg";
+      document.querySelector('.icone').src = "./Imagens/Icone-menu-fechar.svg";
   }
 }
